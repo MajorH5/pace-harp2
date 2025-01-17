4e61bcb4a9e46300a93f8077f03c4fc51c927465
Habib <73318032+MajorH5@users.noreply.github.com>
2025-01-16 23:12:10 -0500

    move branding logos

--- layouts/branding.py
+++ layouts/branding.py
@@ -10,8 +10,8 @@ def create_branding():
         id="branding-container",
         style={
             "position": "absolute",
-            "bottom": "10px",
-            "right": "10px",
+            "top": "10px",
+            "left": "60px",
             "backgroundColor": "rgba(255, 255, 255, 0.9)",
             "borderRadius": "10px",
             "boxShadow": "0px 4px 8px rgba(0, 0, 0, 0.2)",
