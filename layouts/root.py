1c8553cabe019e024108971671632932a4fee13e
Habib <73318032+MajorH5@users.noreply.github.com>
2025-01-17 22:28:47 -0500

    use cleaner map overlays

--- layouts/root.py
+++ layouts/root.py
@@ -14,11 +14,13 @@ def create_root():
     return html.Div(
         children=[
             dl.Map(id="map", center=[56, 10], zoom=7, children=[
-                dl.TileLayer(),
-                dl.TileLayer(id="tc", opacity=0.5),
+                dl.TileLayer(url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"),
+                dl.TileLayer(url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}{r}.png"),
+                dl.TileLayer(url="",id="tc", opacity=0.5),
                 dl.Colorbar(id="cbar", width=150, height=20, style={
                     "margin-left": "40px"}, position="bottomleft"),
-                dl.LayerGroup(id="layer-group")
+                dl.LayerGroup(id="layer-group"),
+                dl.AttributionControl(prefix='<a href="https://carto.com/basemaps" target="_blank">cartocdn</a>')
             ], style={"width": "100%", "height": "100%"}),
             # create_granule_data_view(),
             create_data_controller(),
