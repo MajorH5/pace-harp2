72f7c53828fd76d55e2a8fc55bdcdd5ae89777fa
Habib Aina <habibaina29@gmail.com>
2025-04-15 14:09:25 +0000

    move dash port to config file

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask
 from callbacks import register_callbacks
 from layouts import apply_layout
 
+from config import DASH_PORT
+
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server)
 
@@ -12,4 +14,4 @@ apply_layout(app)  # create client UI
 register_callbacks(app)  # enable ui interactions (server->client callbacks)
 
 if __name__ == '__main__':
-    app.run_server(port=8050)
+    app.run_server(port=DASH_PORT)
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Server config.
 TC_PORT = 4000
+DASH_PORT = 8050
 # TC_HOST = "localhost"
 TC_URL = f"https://demo-gunicorn.esi-cloud.org"
 TC_LOCAL = f"http://localhost:{TC_PORT}"
